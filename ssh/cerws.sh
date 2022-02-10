2c5c9c28fe165e1231624f90e8791b19cdc710d0
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-02-10 18:53:01 +0700

    Create cerws.sh

---
+++ ssh/cerws.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+domain=$(cat /var/lib/datavpn/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+restart
