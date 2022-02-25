14f3f5b65d91ed8fc6434113cf9ceaf0d7e613da
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-02-25 10:19:08 +0000

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -73,4 +73,5 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${WILD_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
-echo $SUB_DOMAIN > /var/lib/datavpn/domain
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
